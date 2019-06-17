1d285956b0d562b19ce793889a13066a2db7e67b
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-17 13:30:11 -0700

    extended server.js start function to accept route function as a parameter

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const http = require('http');
 const url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
         const pathName = url.parse(request.url).pathname;
         console.log('Request for ' + pathName + ' received!');
+
+        route(pathName);
+
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
         response.end();
     }
     
     http.createServer(onRequest).listen(8888);
-    
     console.log('Server has started.');
 }
 
