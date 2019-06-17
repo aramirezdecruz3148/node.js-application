8a87ea2e47309a82e124f2216f46bb210a1e8c9a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-17 13:22:45 -0700

    added logic to the server.js function to find out which URL path the browser requested

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const url = require('url');
 
 function start() {
     function onRequest(request, response) {
-        console.log('Request received!');
+        const pathName = url.parse(request.url).pathname;
+        console.log('Request for' + pathName + 'received!');
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
         response.end();
