37fc7a7eb1ce29f078f185a1c879c335e1f3451c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-17 16:17:57 -0700

    ammended the start function in requestHandlers again using new node.js module child_process

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,14 @@
+let exec = require('child_process').exec;
+
 function start() {
     console.log('Request handler "start" was called!');
+    let content = 'empty';
 
-    function sleep(miliSeconds) {
-        const startTime = new Date().getTime();
-        while (new Date().getTime() < startTime + miliSeconds);
-    }
+    exec('ls -lah', function (error, stdout, stderr ) {
+        content = stdout;
+    });
 
-    sleep(10000);
-    return 'Hello Start';
+    return content;
 }
 
 function upload() {
