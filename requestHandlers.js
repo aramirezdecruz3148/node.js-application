8f3d38a08e231582f2a67004bda3a309c50a82fb
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-17 18:06:42 -0700

    woah, it has been a long day, and I missed a whole section of code in my requestHandlers upload function

--- requestHandlers.js
+++ requestHandlers.js
@@ -30,12 +30,18 @@ function upload(response, request) {
     console.log('about to parse');
     form.parse(request, function(error, fields, files) {
         console.log('parsing done');
-    });
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('received image: <br/>');
-    response.write('<img src="/show" />');
-    response.end();
+        fs.rename(files.upload.path, '/tmp/test.png', function(error) {
+            if(error) {
+                fs.unlink('/tmp/test.png');
+                fs.rename(files.upload.path, '/tmp/test.png');
+            }
+        });
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('received image: <br/>');
+        response.write('<img src="/show" />');
+        response.end();
+    });
 }
 
 function show(response) {
