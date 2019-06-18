7041e2e98bd4f43a0c48f62b1924e97ea7b10560
aramirezdecruz3148 <leigh3148@gmail.com>
2019-06-17 17:12:57 -0700

    passed postData as an argument in both start and upload functions, as well as in the response of the upload function

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,6 @@
 let exec = require('child_process').exec;
 
-function start(response) {
+function start(response, postData) {
     console.log('Request handler "start" was called!');
 
     const body = '<html>'+
@@ -20,10 +20,10 @@ function start(response) {
         response.end();
 }
 
-function upload(response) {
+function upload(response, postData) {
     console.log('Request handler "upload" was called!');
     response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello Upload');
+    response.write('You have sent: ' + postData);
     response.end();
 }
 
